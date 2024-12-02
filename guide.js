aa5b6734a0b2aa3bead698324e93e705b0e13966
Monish-Namdeo <monish12namdeo@gmail.com>
2024-12-02 21:18:13 +0530

    added guide.js file

---
+++ guide.js
@@ -0,0 +1,9 @@
+//  my git commands
+//  git add .   
+//  git commit -m "first commit"
+//  git branch -M main
+//  git remote add origin https://github.com/your-username/your-repo-name.git
+//  git push -u origin main
+//  git branch -a
+//  git checkout -b feature/new-feature
+//  git add .
